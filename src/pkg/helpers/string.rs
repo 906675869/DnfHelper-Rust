72b1538a62c00480a806132e34b1463dc8a83277
qiuapeng <qiuapeng@vchangyi.com>
2023-05-09 14:05:12 +0800

    ascii_to_unicode,unicode_to_string

--- src/pkg/helpers/string.rs
+++ src/pkg/helpers/string.rs
@@ -1,20 +1,22 @@
 fn ascii_to_unicode(s: &str) -> Vec<u8> {
-    let mut new_bytes = Vec::new();
+    let mut bytes_arr = Vec::new();
     for c in s.chars() {
-        let code_point = c as u32;
-        new_bytes.extend_from_slice(&(code_point.to_be_bytes()));
+        let hex_int = c as u32;
+        let mut hex_bytes = hex_int.to_le_bytes().to_vec(); // 将整数转换为小端字节序的字节数组，并转换为 Vec<u8>
+        bytes_arr.append(&mut hex_bytes); // 将字节数组追加到结果数组中
     }
-    new_bytes
+    bytes_arr
 }
 
-fn unicode_to_string(b: &[u8]) -> String {
-    let mut rune_array = Vec::new();
-    for i in (0..b.len()).step_by(2) {
-        if b[i] == 0 && b[i + 1] == 0 {
+fn unicode_to_ascii(ls: &[u8]) -> String {
+    let mut text = String::new();
+    for i in (0..ls.len()).step_by(2) {
+        if ls[i] == 0 && ls[i + 1] == 0 {
             break;
         }
-        let code_point = ((b[i] as u32) << 8) | (b[i+1] as u32);
-        rune_array.push(std::char::from_u32(code_point).unwrap());
+        let a = (ls[i+1] as u32) << 8;
+        let b = ls[i] as u32;
+        text.push(std::char::from_u32(a | b).unwrap());
     }
-    rune_array.iter().collect()
+    text
 }
